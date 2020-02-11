30774fe4b709b8b2c4e25c380a6ab99455c061e4
loveminpic <minji0479@naver.com>
2020-02-11 16:58:03 -0500

    set up test mp3 file and checked bug

--- Scribe/Controller/ViewController.swift
+++ Scribe/Controller/ViewController.swift
@@ -12,12 +12,50 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    @IBOutlet weak var transcriptionTextField: UITextView!
+    
+    var audioPlayer:AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       
+        activitySpinner.isHidden = true
     }
-
-
+        func requestSpeechAuth() {
+            SFSpeechRecognizer.requestAuthorization {authStatus  in
+                if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                    if let path = Bundle.main.url(forResource: "test", withExtension: "m4a"){
+                        do {
+                            let sound = try AVAudioPlayer(contentsOf: path)
+                            self.audioPlayer = sound
+                            sound.play()
+                            
+                        } catch {
+                            print("ERROR!")
+                        }
+                        
+                        let recognzier = SFSpeechRecognizer()
+                        let request = SFSpeechURLRecognitionRequest(url: path)
+                        recognzier?.recognitionTask(with: request) {(result,error) in
+                            if let error = error {
+                                print("There was an error: \(error)")
+                            } else {
+                                print(result?.bestTranscription.formattedString as Any)
+                            }
+                        }
+                        
+                    }
+                    
+                }
+            }
+        }
+      
+    @IBAction func playBtnpress(_ sender: Any) {
+        
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+    }
+    
 }
 
